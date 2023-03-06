d6acc521aa4dbd08e461bd08c896ecc7b8cabc2e
Carlos Cassol <carloscassolfilho@gmail.com>
2023-03-05 22:43:00 -0300

    Permissao de acesso ao calendario

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,13 +1,11 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from '../../lib/axios'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
-  const session = useSession()
-
   // async function handleRegister() {
 
   // }
@@ -36,8 +34,6 @@ export default function Register() {
             <ArrowRight />
           </Button>
         </ConnectItem>
-
-        <Text>{JSON.stringify(session.data)}</Text>
 
         <Button type="submit">
           Próximo passo
